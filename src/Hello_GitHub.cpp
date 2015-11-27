ae1bc06f6d223df081caaf495767456058461890
FRAN <francis.butra@gmail.com>
2015-11-27 11:27:31 +0100

    Buenas chicos, he comenzado a hacer el repositorio, espero que podáis continuar con ello.

---
+++ src/Hello_GitHub.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hello_GitHub.cpp
+// Author      : Fran
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!Hello it’s me - Fran Butragueño!!" << endl; // prints !!Hola Mundo!!
+	return 0;
+}
