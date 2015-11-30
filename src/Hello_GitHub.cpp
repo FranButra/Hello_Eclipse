34adcf410a1c26bdbddfcfb57833f12644814468
B0gdar <cosa.cor.san@gmail.com>
2015-11-30 12:02:38 +0100

    Adición nueva línea hello world de Miguel Cordero (línea 21)

--- src/Hello_GitHub.cpp
+++ src/Hello_GitHub.cpp
@@ -18,6 +18,6 @@ int main() {
 	cout << "!!Hello it's me - Diego González Morín" << endl; // prints !!Hola Mundo!!
 	cout << "!!Hello it's me - David Gozalo" << endl; // prints david gozalo
 	cout << "!!Hello it's me - Ramón Contreras" << endl; // prints Ramón contreras
-
+    cout << "Hello it's me - Miguel Cordero Sánchez(B0gdar)"<< endl;// Prints Hello it's me - Miguel Cordero Sánchez(B0gdar)
 	return 0;
 }
