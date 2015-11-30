dbee6df2407fbd4299b2e2d03883739356fe9e93
ramoncontrerastapetado <ramoncontrerastapetado@gmail.com>
2015-11-30 01:21:35 +0100

    Ramon Contreras
    Añadir

--- src/Hello_GitHub.cpp
+++ src/Hello_GitHub.cpp
@@ -17,6 +17,7 @@ int main() {
 	cout << "!!Hello it's me - Miguel Carretero (altmbc)!!" << endl; // prints !!Hola Mundo!!
 	cout << "!!Hello it's me - Diego González Morín" << endl; // prints !!Hola Mundo!!
 	cout << "!!Hello it's me - David Gozalo" << endl; // prints david gozalo
+	cout << "!!Hello it's me - Ramón Contreras" << endl; // prints Ramón contreras
 
 	return 0;
 }
